6ffb9a8362acb829e938ca589f9a0f3a8fb407e6
Anand <anandjayakumar2001@gmail.com>
2025-11-16 10:08:01 +0530

    initial commit: add database configs, add all initial configs, add entities for shortlinks and user

--- obj/Debug/net9.0/UrlShortenerApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/UrlShortenerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortenerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0fba2c8dfff901a0fb754b23854dcda77b7917")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19688ed54ce93484d8ffc314f478899bc4c1770")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortenerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortenerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
